c6b39936d84977f791231384b9d8818ab3410256
hyeeekyung <hhgyeong87@gmail.com>
2025-10-15 14:53:15 +0900

    Update call_collect.py

--- scripts/call_collect.py
+++ scripts/call_collect.py
@@ -4,10 +4,8 @@ from aegis_client import trigger_collect
 
 if __name__ == "__main__":
     data = trigger_collect(
-        # AEGIS 서버는 게이트웨이의 /aegis 밑으로 노출
-        server_host="http://<gateway-host>:9000/aegis",
-        # Collector는 /collector 밑으로 노출
-        collector_api="http://<gateway-host>:9000/collector",
+        server_host="http://211.44.183.248:9000/aegis",
+        collector_api="http://211.44.183.248:9000/collector",
         only_detected=True,
     )
     print(json.dumps(data, ensure_ascii=False, indent=2))
