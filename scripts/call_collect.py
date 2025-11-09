fea66cdc0e327175edb4394f3a516502f3245345
wonjjani <wonjjani@gmail.com>
2025-11-09 20:39:51 +0900

    Feat:ha

--- scripts/call_collect.py
+++ scripts/call_collect.py
@@ -4,8 +4,8 @@ from aegis_client import trigger_collect
 
 if __name__ == "__main__":
     data = trigger_collect(
-        server_host="http://211.44.183.248:9000/aegis",
-        collector_api="http://211.44.183.248:9000/collector",
+        server_host="http://43.202.228.52:9000/aegis",
+        collector_api="http://43.202.228.52:9000/collector",
         only_detected=True,
     )
     print(json.dumps(data, ensure_ascii=False, indent=2))
